4956e33759108237b1a8db24c3ae1f5112b097c3
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-06-22 02:41:37 +0800

    Fix: install opencv 4.x.x version NOT 3.x.x in install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,21 @@
 # install flask
 pip3 install flask
 
+# install opencv
+# important:
+#           Please check your opencv is 4.x.x
+#           If you are 3.x.x will get error!
+pip3 install opencv-python
+sudo apt install libatlas-base-dev
+
+# install apriltag
+pip3 install pupil_apriltags
+
 # install numpy
 pip3 install numpy
+
+# 
+pip3 install imutils
 
 # install nodejs and npm
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
